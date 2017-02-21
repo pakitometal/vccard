24d8e9e432e04b05683c361c6a3245ae8c2a7159
Pakito <francisco.liceran@mobusi.com>
2017-02-21 18:12:20 +0100

    Fixes

--- php/config/conf.consts.php
+++ php/config/conf.consts.php
@@ -1,5 +1,4 @@
 <?php
-const DB_FILE = $_SERVER['DOCUMENT_ROOT'].'/db/coords.db';
-const DEST_MAIL = '';
-const FROM_MAIL = '';
-
+const VCCARD_DB_FILE = $_SERVER['DOCUMENT_ROOT'].'/db/coords.db';
+const VCCARD_DEST_MAIL = '';
+const VCCARD_FROM_MAIL = '';
--- php/vccard.php
+++ php/vccard.php
@@ -3,7 +3,7 @@
 require_once('includes/inc.sqlite3.php');
 include_once('config/conf.consts.php');
 
-function response($status){
+function response( $status ) {
 	echo json_encode([ 'status' => (bool)$status ]);
 	die;
 }
@@ -14,7 +14,7 @@ if ( !(isset($_POST['pos']) && false !== preg_match('#^(50|[1-4]{1}[0-9]{1}|[1-9
 }
 
 // Open connection with DB
-if ( !$conn = sqlite3_open(DB_NAME, SQLITE3_OPEN_READONLY, $_POST['loc']) ) {
+if ( !$conn = sqlite3_open(VCCARD_DB_FILE, SQLITE3_OPEN_READONLY, $_POST['loc']) ) {
 	response(false);
 }
 
@@ -30,8 +30,8 @@ if ( !($result && $result['val']) ) {
 }
 
 // Send value via email
-$to_mail = DEST_MAIL;
-$from_mail = FROM_MAIL;
+$to_mail   = isset( $_SERVER['VCCARD_DEST_MAIL'] ) ? $_SERVER['VCCARD_DEST_MAIL'] : VCCARD_DEST_MAIL;
+$from_mail = isset( $_SERVER['VCCARD_DEST_MAIL'] ) ? $_SERVER['VCCARD_FROM_MAIL'] : VCCARD_FROM_MAIL;
 
 $subject = 'Answer ('.date('Ymd H:i:s').')';
 $message = $result['val'];
@@ -41,4 +41,3 @@ $headers_array = [
 ];
 $headers = implode("\r\n", $headers_array);
 response(mail($to_mail, $subject, $message, $headers));
-
